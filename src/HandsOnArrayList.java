cb8fcf27528bfc249bdd578a186052d53bb48d19
josias-25 <josnkina@gmail.com>
2024-12-11 09:52:41 -0500

    ArrayList

--- src/HandsOnArrayList.java
+++ src/HandsOnArrayList.java
@@ -9,10 +9,11 @@ public class HandsOnArrayList {
         Students.add("Bob");
         Students.add("Mary");
         System.out.println(Students);
-
+// remove  to remove the name  of the student
         Students.remove("John");
         System.out.println(Students);
 
+        // collections make the student name in alphabetic order.
         Collections.sort(Students);
         System.out.println(Students);
     }
